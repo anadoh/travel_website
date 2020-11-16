b151695e6d5fd59353f6bb0b681cb06a8ff09c00
anadoh <68846949+anadoh@users.noreply.github.com>
2020-11-16 09:40:15 +0100

    loading icon spinner

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.querySelector('.hamburger-menu').addEventListener('click',() =>{
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add('display')
+    }, 2000);
+};
+
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('change');
 });
 
 document.querySelector('.scroll-btn').addEventListener('click', () => {
-    document.querySelector('html').style.scrollBehavior='smooth';
+    document.querySelector('html').style.scrollBehavior = 'smooth';
     setTimeout(() => {
-        document.querySelector('html').style.scrollBehavior='unset';
-    },1000)
-})
+        document.querySelector('html').style.scrollBehavior = 'unset';
+    }, 1000)
+});
+
+
